4e58327498a590ee993f98db2e7a9c783d0ac3ed
Po-Chu Hsu <tonypottera@gmail.com>
2022-12-19 15:01:01 +0900

    new benchmark

--- benchmark.sh
+++ benchmark.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-U=(10 10)
-P=(10 100)
+U=(10 10 100 100)
+P=(10 100 10 100)
 
 # for U in 10 20 30 40 50 60 70 80 90 100
 # for U in 100 200 300 400 500 600 700 800 900 1000
@@ -11,3 +11,7 @@ do
     ./main.py -u ${U[i]} -p ${P[i]} &> log/${U[i]}U_${P[i]}P.log
     echo "`date` U = ${U[i]}, P = ${P[i]} end"
 done
+
+git add .
+git commit -m "new benchmark"
+git push
